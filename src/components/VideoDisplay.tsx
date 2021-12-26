7ae28654cc3fdcba4a3654c299cfdea564126c7f
hannasophian <90692357+hannasophian@users.noreply.github.com>
2021-12-26 18:18:44 +0000

    Revert "Revert "Created basic app yay""

---
+++ src/components/VideoDisplay.tsx
@@ -0,0 +1,15 @@
+export default function VideoDisplay(props: { vidIDs: string[] }): JSX.Element {
+  let showVids;
+  if (props.vidIDs.length !== 0) {
+    showVids = props.vidIDs.map((id) => (
+      <iframe
+        // key = {id}
+        title={id}
+        width="420"
+        height="315"
+        src={`https://www.youtube.com/embed/${id}`}
+      ></iframe>
+    ));
+  }
+  return <>{showVids}</>;
+}
---
+++ src/utils/Video.tsx
@@ -0,0 +1,7 @@
+export interface Video {
+  id: number;
+  youtube_id: string;
+  duration: number;
+  tag: string;
+  level: number;
+}
