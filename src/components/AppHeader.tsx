e14a6e8d408761bfe31c725294513a6c66f3f945
Hanna <sophianhanna@gmail.com>
2021-12-30 00:39:43 +0000

    Added css to header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,9 +2,11 @@ export default function AppHeader(): JSX.Element {
   return (
     <div className="App-Header">
       <div>
-        <h1>YogaFit</h1>
-        {/* <br /> */}
-        <p>Get a custom workout to relax and unwind</p>
+        <div className="header-box">
+          <h1>YogaFit</h1>
+          {/* <br /> */}
+          <p>Get a custom workout to relax and unwind</p>
+        </div>
         <small>Scroll down to continue!</small>
       </div>
     </div>
